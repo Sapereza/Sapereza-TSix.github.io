d444d22350583d49368131cfbde81cf93f8a038f
Sapereza <s.perezanderson@gmail.com>
2022-07-28 09:58:04 -0400

    This 5 page site is still in the beginner stages. I have some new code in my html in the footer. The problem is that I spent a few hours trying to make that code responsive. so that I have a different layout in the footer for when the screen is large like on a laptop or desktop. However, its not quite working out. Only the change for md screen size worked how I wanted it to.

--- Molex.js
+++ Molex.js
@@ -1,3 +1,5 @@
+// THis function is for the menu icon on the navigation bar for md to sm screen sizes.
+
 function menu() {
   var x = document.getElementById("links");
   if (x.style.display === "block") {
@@ -6,3 +8,24 @@ function menu() {
     x.style.display = "block";
   }
 }
+
+const accordionHeader = document.querySelectorAll(".accordion-header");
+    accordionHeader.forEach((header) => {
+    header.addEventListener("click", function () {
+        const accordionContent = header.parentElement.querySelector(".accordion-content");
+        let accordionMaxHeight = accordionContent.style.maxHeight;
+
+        // Condition handling
+        if (accordionMaxHeight == "0px" || accordionMaxHeight.length == 0) {
+        accordionContent.style.maxHeight = `${accordionContent.scrollHeight + 32}px`;
+        header.querySelector(".fas").classList.remove("fa-plus");
+        header.querySelector(".fas").classList.add("fa-minus");
+        header.parentElement.classList.add("bg-indigo-50");
+        } else {
+        accordionContent.style.maxHeight = `0px`;
+        header.querySelector(".fas").classList.add("fa-plus");
+        header.querySelector(".fas").classList.remove("fa-minus");
+        header.parentElement.classList.remove("bg-indigo-50");
+        }
+    });
+    });
